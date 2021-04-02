28f6968a8af2d200d07418ba71e660847f87ef3b
mozky <moz@mozky.dev>
2021-04-02 15:27:08 -0700

    Remove all Canvas code

--- src/app.js
+++ src/app.js
@@ -1,20 +1,5 @@
-import './style.scss'
 
-// Basic CSS
-//import './css/bootstrap.css'
-//import './style.css'
-
-// One-page CSS
-import './one-page/css/fonts.css'
-import './css/font-icons.css'
-import './one-page/css/et-line.css'
-import './css/animate.css'
-import './one-page/onepage.css'
-
-// HTML Fragments
-import mainHtml from './main.html'
-
-console.log('Hi from index.js!')
+console.log('From index.js!')
 
 setTimeout(() => {
   const bodyRef = document.body

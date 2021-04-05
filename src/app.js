35af9cca56ef90416e01d276fc3891a16cfcf881
mozky <moz@mozky.dev>
2021-04-04 18:29:00 -0600

    Add menus toggle

--- src/app.js
+++ src/app.js
@@ -2,3 +2,25 @@ import 'normalize.css'
 import './sass/styles.scss'
 
 console.log('From index.js!')
+
+const ferboButton = document.getElementById('ferbo-toggle')
+
+ferboButton.addEventListener('click', function() {
+  const menu = document.getElementById('ferbo-menu')
+  if (menu.classList.contains("show")) {
+    menu.classList.remove("show")
+  } else {
+    menu.classList.add("show")
+  }
+})
+
+const mexhaButton = document.getElementById('mexha-toggle')
+
+mexhaButton.addEventListener('click', function() {
+  const menu = document.getElementById('mexha-menu')
+  if (menu.classList.contains("show")) {
+    menu.classList.remove("show")
+  } else {
+    menu.classList.add("show")
+  }
+})

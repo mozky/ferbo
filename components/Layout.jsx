86934de4f73ddb667545e65eb18f61a831ce0dee
mozky <moz@mozky.dev>
2021-04-12 20:33:20 -0500

    More animations

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -18,13 +18,13 @@ const Layout = ({ props, children }) => {
     
       col1 && col1.classList.add("visible")
       col2 && col2.classList.add("visible")
-    }, 3000)
+    }, 4000)
   }, [router])
 
   useEffect(() => {
     setTimeout(() => {
       toggleLoading(false)
-    }, 200)
+    }, 2000)
   }, [])
 
   const routeSite = router.pathname.length > 1 ? router.pathname.substring(1, router.pathname.lastIndexOf("/")) : false
@@ -38,21 +38,21 @@ const Layout = ({ props, children }) => {
     footer: 
       <>
         <SocialIcons/>
-        <MenuButtom big={openMenu === 'ferbo'} menuName="ferbo" onClick={() => toggleOpenMenu(openMenu === 'ferbo' ? false : 'ferbo')} width={60} height={110} />
-        <MenuButtom big={openMenu === 'mexha'} menuName="mexha" onClick={() => toggleOpenMenu(openMenu === 'mexha' ? false : 'mexha')} width={90} height={70} />
+        <MenuButtom key="ferbo-menu" big={openMenu === 'ferbo'} menuName="ferbo" onClick={() => toggleOpenMenu(openMenu === 'ferbo' ? false : 'ferbo')} width={60} height={110} />
+        <MenuButtom key="mexha-menu" big={openMenu === 'mexha'} menuName="mexha" onClick={() => toggleOpenMenu(openMenu === 'mexha' ? false : 'mexha')} width={90} height={70} />
       </>
   }
 
   const insideLayout = {
     col5: routeSite === 'ferbo' ? <FerboMenu showMenu="true" /> : <MexhaMenu showMenu="true" />,
-    footer: <MenuButtom big backStyle menuName={routeSite} onClick={() => router.push('/')} width={routeSite === 'ferbo' ? 60 : 90} height={routeSite === 'ferbo' ? 110 : 70} />
+    footer: <MenuButtom key={routeSite + "-menu"} big backStyle menuName={routeSite} onClick={() => router.push('/')} width={routeSite === 'ferbo' ? 60 : 90} height={routeSite === 'ferbo' ? 110 : 70} />
   }
 
   const layout = routeSite ? insideLayout : homeLayout
 
   return (
     <div className={`container ${routeSite ? 'inside_page' : ''}`}>
-      <div id="loading" className={showLoading ? '' : 'hide'}><div id="top-half"></div><div id="bottom-half"></div></div>
+      <div id="loading" className={`${routeSite ? 'inside_page ': ' '} ${showLoading ? '' : 'hide'}`}><div id="top-half"></div><span>"Los detalles no son los detalles. Los detalles son el diseno" Charles Eames</span><div id="bottom-half"></div></div>
       <main>{children}</main>
       {layout.col1 && <div id="col1">{layout.col1}</div>}
       {layout.col2 && <div id="col2">{layout.col2}</div>}
